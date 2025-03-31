cd9a4774eb55620abcfd7fb0b8cd91474a1e0b1c
Yassinebe963 <yassinebe963@gmail.com>
2025-03-31 12:23:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,3 +15,19 @@ def exercices():
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
+  from flask import Flask
+from flask import render_template
+from flask import json                                                                                                                                     
+app = Flask(__name__)                                                                                                                  
+
+
+@app.route('/<int:valeur>')
+def exercice(valeur):
+    etoiles = ''
+    for i in range(valeur):
+        etoiles += '*'
+    return etoiles #comm
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
