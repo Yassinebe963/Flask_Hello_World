1a81e0842f431ecf4f288a9d93149131ec6f5434
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:51:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <pre id="pyramide"></pre>
-  <script>
-    let n = 5;
-    let output = "";
-    for (let i = 1; i <= n; i++) {
-      output += " ".repeat(n - i) 
-               + Array.from({length: i}, (_, j) => j + 1).join('') 
-               + Array.from({length: i - 1}, (_, j) => i - 1 - j).join('') 
-               + "\n";
-    }
-    document.getElementById("pyramide").textContent = output;
-  </script>
-</body>
-</html>
+  etoiles = ''
+    for j in range(valeur):
+        for i in range(valeur-j):
+            etoiles += '+'   
+        for k in range(j+1):
+            etoiles += '*'
+        etoiles += '<br>'
+    return etoiles
+
