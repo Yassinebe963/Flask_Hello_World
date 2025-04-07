de2be1d3d058f63c81b8b868f3ff09121bd23991
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 14:54:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,14 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/<path:valeurs>')
-def exercice(valeurs):
-    liste_nombres = valeurs.split('/')
-    liste_nombres = [int(n) for n in liste_nombres]
-    resultat = 0
-    for n in liste_nombres:
-        resultat = resultat + n
-    return str(resultat)
+@app.route('/<path:numbers>')
+def find_max(numbers):
+    try:
+        # Convertir les segments en nombres
+        numbers_list = [int(n) for n in numbers.split('/')]
+        
+        if not numbers_list:
+            return "Aucun nombre fourni", 400
+        
+        # Trouver le maximum manuellement
+        maximum = numbers_list[0]
+        for num in numbers_list[1:]:
+            if num > maximum:
+                maximum = num
+                
+        return f"Le maximum parmi {numbers_list} est {maximum}"
+    
+    except ValueError:
+        return "Tous les segments doivent être des nombres valides", 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
