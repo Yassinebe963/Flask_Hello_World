2035962337b44bf6434209835aeff4ba593b0109
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:36:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,3 @@
-n = int(input("10 : "))
-
+n = 5
 for i in range(1, n + 1):
-    print(' ' * (n - i), end='')
-    # Partie montante
-    for j in range(1, i + 1):
-        print(j, end='')
-    # Partie descendante
-    for j in range(i - 1, 0, -1):
-        print(j, end='')
-    print()  # Retour à la ligne
+    print(' ' * (n - i) + ''.join(map(str, range(1, i + 1))) + ''.join(map(str, range(i - 1, 0, -1))))
