7b93233106dc043c71128b6f7cadac7ae8f85aa9
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 11:07:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,31 +1,17 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def formulaire():
-    return '''
-    <form action="/suite" method="get">
-        Entrez un nombre : <input type="number" name="valeur" min="2" required>
-        <input type="submit" value="Générer">
-    </form>
-    '''
-
-@app.route('/suite')
-def calcul_suite():
-    try:
-        n = int(request.args.get('valeur', 2))
-    except ValueError:
-        return "Veuillez entrer un nombre valide", 400
+@app.route('/<int:n>')
+def fibo(n):
+    a, b = 0, 1
+    res = str(a)  # Initialisation avec le premier terme
     
-    if n < 2:
-        return "Le nombre doit être au moins 2", 400
+    for _ in range(1, min(n, 50)):  # Limité à 50 termes pour la sécurité
+        res += f", {b}"
+        a, b = b, a + b
     
-    suite = [0, 1]
-    for i in range(2, n):
-        suite.append(suite[-1] + suite[-2])
-    
-    return f"Suite pour n={n} : {', '.join(map(str, suite))}"
+    return res
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
