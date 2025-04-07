88ced5811748093b416b43168b66071328554626
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 11:14:41 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/<int:n>')
 def fibo(n):
-    res = ', '.join(str(b := a + b) if i else str(a) for i, (a, b) in enumerate([(0, 1)] * min(n, 50)))
+    a, b = 0, 1
+    res = str(a)  # Initialisation avec le premier terme
+    
+    for _ in range(1, min(n, 50)):  # Limité à 50 termes pour la sécurité
+        res += f", {b}"
+        a, b = b, a + b
+    
     return res
 
 if __name__ == '__main__':
