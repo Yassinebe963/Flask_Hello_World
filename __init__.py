66b40b5eb2cc98e837735ffcc1b71c52327b6755
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:49:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,17 @@
-from flask import Flask
-
-app = Flask(name)
-
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    pyramide = ''
-    for i in range(1, valeur + 1):
-        ligne = ''
-        ligne += ' ' * (valeur - i)
-        for j in range(1, i + 1):
-            ligne += str(j)
-        for j in range(i - 1, 0, -1):
-            ligne += str(j)
-        pyramide += ligne + '\n'
-    return f"<pre>{pyramide}</pre>"
-
-if name == "main": 
-    app.run(debug=True)
+<!DOCTYPE html>
+<html>
+<body>
+  <pre id="pyramide"></pre>
+  <script>
+    let n = 5;
+    let output = "";
+    for (let i = 1; i <= n; i++) {
+      output += " ".repeat(n - i) 
+               + Array.from({length: i}, (_, j) => j + 1).join('') 
+               + Array.from({length: i - 1}, (_, j) => i - 1 - j).join('') 
+               + "\n";
+    }
+    document.getElementById("pyramide").textContent = output;
+  </script>
+</body>
+</html>
