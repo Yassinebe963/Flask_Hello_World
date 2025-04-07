e500503e8d3151c169ff2b724842ff864642a2cb
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:40:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+
+print("Content-Type: text/html\n\n")
 n = 5
 for i in range(1, n + 1):
-    print(' ' * (n - i) + ''.join(map(str, range(1, i + 1))) + ''.join(map(str, range(i - 1, 0, -1))))
+    print(' ' * (n - i) + ''.join(map(str, range(1, i + 1))) + ''.join(map(str, range(i - 1, 0, -1))) + "<br>")
