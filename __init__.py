a87bd70f7c7c040941c330259442cf7ce3c6cf2a
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:28:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    etoiles = ''
-    for i in range(1, valeur + 1):
-        etoiles += ' ' * (valeur + i) + '*' * i + '<br>'
-    return etoiles
-
-if __name__ == "__main__":
-    app.run(debug=True)
+n = 5
+for i in range(1, n + 1):
+    # Espaces à gauche pour centrer
+    print(' ' * (n - i), end='')
+    
+    # Partie croissante (1 à i)
+    for j in range(1, i + 1):
+        print(j, end='')
+    
+    # Partie décroissante (i-1 à 1)
+    for j in range(i - 1, 0, -1):
+        print(j, end='')
+    
+    # Nouvelle ligne
+    print()
