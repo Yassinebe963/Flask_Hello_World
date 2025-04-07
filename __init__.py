d52410db60bdf2d4a73e3e72c4650f9a7f400dfd
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 10:11:17 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,19 @@
-from flask import Flask, render_template_string
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask(name)
 
-@app.route('/')
-def home():
-    return "Ajoutez un nombre à l'URL (ex: /5)"
+@app.route('/<int:valeur>')
+def exercice(valeur):
+    pyramide = ''
+    for i in range(1, valeur + 1):
+        ligne = ''
+        ligne += ' ' * (valeur - i)
+        for j in range(1, i + 1):
+            ligne += str(j)
+        for j in range(i - 1, 0, -1):
+            ligne += str(j)
+        pyramide += ligne + '\n'
+    return f"<pre>{pyramide}</pre>"
 
-@app.route('/<int:taille>')
-def pyramide(taille):
-    # Limiter la taille à 20 pour éviter les abus
-    taille = min(taille, 20)
-    
-    # Générer la pyramide
-    lignes = []
-    for i in range(1, taille + 1):
-        espaces = ' ' * (taille - i)
-        gauche = ''.join(map(str, range(1, i + 1)))
-        droite = ''.join(map(str, range(i - 1, 0, -1)))
-        lignes.append(espaces + gauche + droite)
-    
-   
-    
-
-if __name__ == '__main__':
-    app.run()
+if name == "main":
+    app.run(debug=True)
