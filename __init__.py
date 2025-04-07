637f1e057e74c02bad683fd1a43bb960d88575c2
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:53:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,6 @@
   etoiles = ''
     for j in range(valeur):
         for i in range(valeur-j):
-            etoiles += '+'   
-        for k in range(j+1):
             etoiles += '*'
         etoiles += '<br>'
     return etoiles
-
