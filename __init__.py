01a5ddeaca3d017444c2420a1433c4b2bc78fb24
Yassinebe963 <yassinebe963@gmail.com>
2025-04-07 09:55:53 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
-  etoiles = ''
-    for j in range(valeur):
-        for i in range(valeur-j):
-            etoiles += '*'
-        etoiles += '<br>'
-    return etoiles
+n = 5  # Vous pouvez modifier cette valeur ou demander à l'utilisateur
+
+for i in range(1, n + 1):
+    # Partie gauche (espaces + nombres croissants)
+    ligne = ' ' * (n - i)  # Espaces pour centrer
+    ligne += ''.join(str(j) for j in range(1, i + 1))  # 1 -> i
+    
+    # Partie droite (nombres décroissants)
+    ligne += ''.join(str(j) for j in range(i - 1, 0, -1))  # i-1 -> 1
+    
+    print(ligne)  # Affiche la ligne complète
